2f7e904b2059ec86e558e9887d4c32255f878a74
phantom-gitc <sarojkumarpadhi01@gmail.com>
2026-07-09 23:40:32 +0530

    Form Handling Class

---
+++ React_Two/src/Web.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Web = () => {
+  return (
+    <div>
+      
+      <input type="text" placeholder='Enter your name :' /> <br></br>
+      <input type="email" placeholder='Enter your email :' /> <br></br>
+      <input type="password" placeholder='Enter your password :' /> <br></br>
+    </div>
+  )
+}
+
+export default Web
--- React_Two/src/main.jsx
+++ React_Two/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import Web from './Web.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Web />
 )
